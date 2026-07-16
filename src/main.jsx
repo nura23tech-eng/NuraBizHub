195fc4b929714a7543e793a85d92794040894bcb
nura23tech-eng <nura23tech@gmail.com>
2026-07-16 15:15:46 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,3 +9,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
