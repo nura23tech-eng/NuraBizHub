d5558386c2f0f228f7e9ec66f72df4e395be0675
nura23tech-eng <nura23tech@gmail.com>
2026-07-16 15:33:02 +0100

    Update FeatureCard.jsx

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,16 +1,19 @@
-export default function FeatureCard({ title, icon }) {
-
+function FeatureCard({ title, description }) {
   return (
-    <div className="bg-white shadow-md rounded-xl p-5">
-
-      <div className="text-3xl">
-        {icon}
-      </div>
-
-      <h3 className="font-bold mt-3">
+    <div className="bg-white rounded-2xl shadow-md p-6 hover:shadow-xl transition duration-300 border">
+      <h3 className="text-xl font-semibold text-slate-800 mb-2">
         {title}
       </h3>
 
+      <p className="text-slate-600 mb-6">
+        {description}
+      </p>
+
+      <button className="w-full bg-blue-900 text-white py-2 rounded-lg hover:bg-blue-800">
+        Open
+      </button>
     </div>
   );
 }
+
+export default FeatureCard;
