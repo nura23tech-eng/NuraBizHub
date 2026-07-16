ade7c88f199141817f2838016c357f668c617665
nura23tech-eng <nura23tech@gmail.com>
2026-07-16 12:43:43 +0100

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+
+  return (
+    <header className="bg-primary text-white p-5">
+
+      <h1 className="text-2xl font-bold">
+        NuraBizHub AI
+      </h1>
+
+      <p className="mt-1">
+        Smart business companion for African SMEs
+      </p>
+
+    </header>
+  );
+}
