06ce4387a96534524497068290d26d7d826430a2
nura23tech-eng <nura23tech@gmail.com>
2026-07-16 16:33:53 +0100

    Update FeatureCard.jsx

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,17 +1,32 @@
-function FeatureCard({ title, description }) {
+function FeatureCard({ title, description, icon, color }) {
   return (
-    <div className="bg-white rounded-2xl shadow-md p-6 hover:shadow-xl transition duration-300 border">
-      <h3 className="text-xl font-semibold text-slate-800 mb-2">
+    <div
+      className={`bg-white rounded-3xl p-6 shadow-sm border border-slate-100 
+      hover:shadow-xl hover:-translate-y-1 transition-all duration-300`}
+    >
+
+      <div
+        className={`w-14 h-14 rounded-2xl flex items-center justify-center 
+        text-3xl mb-5 ${color}`}
+      >
+        {icon}
+      </div>
+
+      <h3 className="text-xl font-bold text-slate-800 mb-2">
         {title}
       </h3>
 
-      <p className="text-slate-600 mb-6">
+      <p className="text-slate-500 leading-relaxed min-h-[50px]">
         {description}
       </p>
 
-      <button className="w-full bg-blue-900 text-white py-2 rounded-lg hover:bg-blue-800">
+      <button
+        className="mt-6 w-full bg-blue-900 text-white py-3 rounded-xl 
+        font-semibold hover:bg-blue-800 transition"
+      >
         Open
       </button>
+
     </div>
   );
 }
